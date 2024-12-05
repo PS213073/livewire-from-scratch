2629b32b6120d16e8f5151b678cc58bdd72b3fe1
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2024-12-05 22:40:35 +0100

    actions with form

--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,6 +1,8 @@
 <div>
-
     <div><h1>Hello from {{ $name }}!</h1></div>
+    <form
+        wire:submit="changeName(document.querySelector('#newName').value)"
+    >
         <div class="mt-2">
             <input
                 id="newName"
@@ -10,9 +12,8 @@
         <div class="mt-2">
             <button class="text-white font-medium rounded-md bg-blue-600 px-4 py-2"
                     type="submit"
-                    wire:click="changeName(document.querySelector('#newName').value)"
             >Greet
             </button>
         </div>
-
+    </form>
 </div>
