dc63dcde37a48abb787df1c1ac4b521bf905aed5
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2024-12-05 22:38:41 +0100

    using actions

--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,3 +1,18 @@
 <div>
-    <h1>Hello from {{ $name }}!</h1>
+
+    <div><h1>Hello from {{ $name }}!</h1></div>
+        <div class="mt-2">
+            <input
+                id="newName"
+                class="block w-full p-4 border rounded-md bg-gray-700"
+                type="text">
+        </div>
+        <div class="mt-2">
+            <button class="text-white font-medium rounded-md bg-blue-600 px-4 py-2"
+                    type="submit"
+                    wire:click="changeName(document.querySelector('#newName').value)"
+            >Greet
+            </button>
+        </div>
+
 </div>
